ad66fd147f9be1cc749300f88c176ee711d881dc
pleichty <pleichty@gmail.com>
2019-05-31 19:02:11 -0500

    WIP - a forest tile isn't being initialzed correctly?

--- Kingdomino/include/tile.hpp
+++ Kingdomino/include/tile.hpp
@@ -46,19 +46,45 @@ public:
 	}
 
     SDL_Texture* getTextureforTile(SDL_Renderer* renderer, SDL_Texture* textures[]){
+		
       if(terrain == Terrain::grass){
         return textures[0 + crownCount];
       }
-
-      else if(terrain == Terrain::forest){
-        return textures[3 + crownCount];
+	  
+	  else if(terrain == Terrain::forest && crownCount == 0){
+        return textures[3];
       }
+	  
+	  else if (terrain == Terrain::forest && crownCount == 1) {
+		  return textures[4];
+	  }
+	  else if (terrain == Terrain::forest && (crownCount != 0 || crownCount != 1)) {
+		  TTF_Font* font;
+
+		  font = TTF_OpenFont("romfs:/resources/fonts/MiniSet2.ttf", 24);
+		  std::string player_2_score_text = "Player 2 Score: " + std::to_string(crownCount);
+
+
+		  SDL_Color textColor = { 255, 255, 255, 0 };
+
+		  SDL_Surface* textSurface = TTF_RenderText_Solid(font, player_2_score_text.c_str(), textColor);
+		  SDL_Texture* text = SDL_CreateTextureFromSurface(renderer, textSurface);
+		  int text_width = textSurface->w;
+		  int text_height = textSurface->h;
+		  SDL_FreeSurface(textSurface);
+		  SDL_Rect renderQuad = { 20, 720 - 30, text_width, text_height };
+		  SDL_RenderCopy(renderer, text, NULL, &renderQuad);
+		  return textures[17];
+	  }
+	  
       else if(terrain == Terrain::wheat){
         return textures[5 + crownCount];
       }
+	  
       else if(terrain == Terrain::water){
         return textures[7 + crownCount];
       }
+	  
       else if(terrain == Terrain::mine){
         return textures[9 + crownCount];
       }
@@ -71,9 +97,12 @@ public:
       else if(terrain == Terrain::p2){
         return textures[18];
       }
+	  
       else{
         return textures[21];
       }
+	  
+		return textures[0];
     }
 
     void printTile(SDL_Renderer* renderer,int x, int y, SDL_Texture* textures[]){
